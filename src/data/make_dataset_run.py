7deebd00e2e76fcdd9fd73ccda6cf643816c78e4
georgezefko <46191927+georgezefko@users.noreply.github.com>
2021-06-11 13:17:18 +0200

    change function name

--- src/data/make_dataset_run.py
+++ src/data/make_dataset_run.py
@@ -5,7 +5,7 @@ from src.data.make_dataset import data
 @click.command()
 @click.argument('data_filepath',  type=click.Path(), default='data')
 
-def make_dataset_command_line(data_filepath):
+def make_dataset_run(data_filepath):
     """ Downloads and stores the MNIST training and test data into
         raw data (../DATA_FILEPATH/MNIST/raw) and into cleaned data ready
         to be analyzed (../DATA_FILEPATH/MNIST/processed).
@@ -16,4 +16,4 @@ def make_dataset_command_line(data_filepath):
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-    make_dataset_command_line()
+    make_dataset_run()
